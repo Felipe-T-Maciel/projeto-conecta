8d0888b1ab303e15d5029bacd304b239d4a71678
FELIPE TOMIO MACIEL <felipe_t_maciel@estudante.sesisenai.org.br>
2023-08-11 12:25:41 -0300

    v1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './core/pages/components/home/home.component';
+import { SidebarComponent } from './core/pages/components/sidebar/sidebar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SidebarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {
+      'sidebar':'#220424',
+    },
+  },
+  plugins: [],
+}
+
