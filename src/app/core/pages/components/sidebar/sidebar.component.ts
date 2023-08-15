e4d10a39a691d48b1033eade432c3bae37345da4
FELIPE TOMIO MACIEL <felipe_t_maciel@estudante.sesisenai.org.br>
2023-08-15 09:40:46 -0300

    v3

--- src/app/core/pages/components/sidebar/sidebar.component.ts
+++ src/app/core/pages/components/sidebar/sidebar.component.ts
@@ -1,15 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, OnDestroy, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent implements OnInit, AfterViewInit, OnDestroy {
 
   constructor() { }
 
+    @ViewChild('monitoredDiv') monitoredDiv !: ElementRef;
+  
+    private resizeObserver !: ResizeObserver;
+  
+
+    ngAfterViewInit() {
+      this.resizeObserver = new ResizeObserver(entries => {
+          for (let entry of entries) {
+              const sidebarWidth = entry.contentRect.width;
+              const windowWidth = window.innerWidth;
+  
+              if ((sidebarWidth / windowWidth) * 100 >= 16) {
+                  this.showText = true;
+              } else {
+                  this.showText = false;
+              }
+          }
+      });
+  
+      this.resizeObserver.observe(this.sidebar.nativeElement);
+  }
+
+  
+
+  
+    ngOnDestroy() {
+      if (this.resizeObserver) {
+        this.resizeObserver.disconnect();
+      }
+    }
+    showText: boolean = false;
+
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    if(this.widthPage >= 1024){
+      this.menu = true;
+    }else{
+      this.menu = false
+    }
+  }
+
   ngOnInit(): void {
+    if(this.widthPage >= 1024){
+      this.menu = true;
+    }else{
+      this.menu = false
+    }
+  }
+
+  widthPage = window.innerWidth
+
+  menu: boolean = false
+  sidebar: any
+
+  menuOpen(){
+    this.menu = !this.menu;
+    this.showText = true
   }
 
 }
